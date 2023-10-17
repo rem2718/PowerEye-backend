2e11bcd47059166007b85152547d6c2f07e3232f
NourAlakhras <NOORALAKHRAS999@HOTMAIL.COM>
2023-10-17 09:50:34 +0300

    user controller

--- web_server/app/controllers/user_controller.py
+++ web_server/app/controllers/user_controller.py
@@ -1,4 +1,4 @@
-# app\controllers\goal_controller.py
+# app\controllers\user_controller.py
 from flask import jsonify
 from app.models.user_model import User
 from app.forms.login_form import LoginForm
@@ -9,54 +9,156 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
 
 
-def login(email, password):
-    form = LoginForm()
 
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
+# Function to validate PowerEye system password
+def validate_power_eye_password(password):
+    # Define complexity criteria
+    if (
+        len(password) < 8
+        or not any(char.isupper() for char in password)
+        or not any(char.islower() for char in password)
+        or not any(char.isdigit() for char in password)
+        or not any(char in '!@#$%^&*()-_+=<>,.?/:;{}[]~' for char in password)
+    ):
+        return False
 
-        user = User.query.filter_by(email=email).first()
-        if user and user.check_password(password):
-            token = user.generate_token()
-            return jsonify({'token': token}), 200
-        else:
-            return jsonify({'message': 'Invalid credentials'}), 401
+    return True
 
-    return jsonify({'message': 'Invalid form submission'}), 400
+# Function to validate Meross credentials (email and password)
+def validate_meross_credentials(email, password):
+    # Implement validation using Meross interface
+    # Return True if valid, False otherwise
+    return True  # Placeholder, replace with actual validation logic
 
-def signup(email, password, meross_password):
-    form = SignupForm()  # Create an instance of the SignupForm
 
-    if form.validate_on_submit():
-        email = form.email.data
-        password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')  # Hash the password
-        meross_password = form.meross_password.data
+def signup(email, power_eye_password, meross_password):
+    try:
+        # Validate PowerEye system password
+        if not validate_power_eye_password(power_eye_password):
+            return jsonify({'error': 'Invalid PowerEye system password.'}), 400
 
-        user = User(email=email, password=password, meross_password=meross_password)
+        # Validate Meross credentials
+        if not validate_meross_credentials(email, meross_password):
+            return jsonify({'error': 'Invalid Meross credentials.'}), 400
+
+        # Create and save the user
+        user = User(
+            email=email,
+            password=power_eye_password,
+            cloud_password=meross_password,
+            cloud_type=CloudType.MERROS
+            # Add other fields as needed
+        )
         user.save()
-        token = user.generate_token()
-        return jsonify({'token': token}), 201
 
-    return jsonify({'message': 'Invalid form submission'}), 400
+        return jsonify({'message': 'User created successfully.'}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
+def login(email, password):
+    try:
+        # Validate if email is provided
+        if not email:
+            return jsonify({'error': 'Email is required.'}), 400
+
+        # Validate if password is provided
+        if not password:
+            return jsonify({'error': 'Password is required.'}), 400
+
+        # Find the user by email
+        user = User.objects(email=email).first()
+
+        if not user:
+            return jsonify({'error': 'Invalid email or password.'}), 401
+
+        # Validate the password
+        if not user.check_password(password):
+            return jsonify({'error': 'Invalid email or password.'}), 401
+
+        # If email and password are valid, generate and return a token
+        token = generate_token(user)
+        
+        return jsonify({'token': token}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 def logout():
-    return jsonify({'message': 'Logout successful'}), 200
+    return jsonify({'message': 'Logged out successfully.'}), 200
 
 
+def get_user_info(user_id):
+    try:
+        # Find the user by ID
+        user = User.objects(id=user_id).first()
+
+        if not user:
+            return jsonify({'message': 'User not found'}), 404
+
+        user_info = {
+            'email': user.email,
+            'username': user.username,
+            'current_month_energy': user.current_month_energy,
+            'energy_goal': user.energy_goal,
+            # Add other user fields as needed
+        }
+
+        return jsonify({'user_info': user_info}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+def update_user_info(user_id, meross_password=None, power_eye_password=None, username=None, profile_picture=None):
+    try:
+        # Find the user by ID
+        user = User.objects(id=user_id).first()
+
+        if not user:
+            return jsonify({'message': 'User not found'}), 404
+
+        # Update user information if provided
+        if meross_password is not None:
+            user.cloud_password = meross_password
+
+        if power_eye_password is not None:
+            if not validate_power_eye_password(power_eye_password):
+                return jsonify({'error': 'Invalid PowerEye system password.'}), 400
+            user.password = power_eye_password
+
+        if username is not None:
+            user.username = username
+
+        if profile_picture is not None:
+            user.profile_picture = profile_picture
+
+        user.save()
+
+        return jsonify({'message': 'User information updated successfully'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
-def get_user_info():
-    # Get user information logic
-    return jsonify({'username': 'JohnDoe', 'profile': 'profile_url', 'goal': '5000'}), 200
+def delete_user(user_id):
+    try:
+        # Find the user by ID
+        user = User.objects(id=user_id).first()
 
-def update_user_info(username, password, meross_password, profile_pic):
-    # Update user information logic
-    return jsonify({'message': 'User info updated successfully'}), 200
+        if not user:
+            return jsonify({'message': 'User not found'}), 404
 
-def delete_user():
-    # Delete user logic
-    return jsonify({'message': 'User deleted successfully'}), 200
+        # Soft delete the user
+        user.is_deleted = True
+        user.save()
+
+        return jsonify({'message': 'User deleted successfully'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 
 def get_goal():
