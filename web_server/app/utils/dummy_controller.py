97a2fcefb80a89df94d65f547d0c7d1f9ffcec10
MayaKhalid <219410523@psu.edu.sa>
2023-10-21 20:56:13 +0300

    init

--- web_server/app/utils/dummy_controller.py
+++ web_server/app/utils/dummy_controller.py
@@ -1,27 +1,18 @@
-from flask import Flask, session
-import asyncio
-from cloud_interface import verify_credentials
+from run import cloud_obj
+from flask import Blueprint, jsonify
 
-app = Flask(__name__)
+dummy_controller = Blueprint('dummy_controller', __name__)
 
-@app.route('/')
-def index():
-    return "Hello, Flask!"
+@dummy_controller.route('/verify')
+def verify_credentials():
+    # Dummy user information for verification
+    user = {
+        'id': 1,
+        'email': 'mayakhalide2001@gmail.com',
+        'password': 'smarthomemaya'
+    }
 
-@app.route('/verify')
-def verify():
-    asyncio.run(run_verification())
-    return "Verification completed"
+    # Call the verification method
+    result = cloud_obj.verify_credentials('meross', user)
 
-async def run_verification():
-    credentials_valid = await verify_credentials(email='mayakhalide2001@gmail.com', password='smarthomemaya')
-    if credentials_valid:
-        print("Credentials are valid.")
-    else:
-        print("Credentials are invalid.")
-
-async def main():
-    await run_verification()
-
-if __name__ == '__main__':
-    app.run()
+    return jsonify({'result': result})
--- web_server/run.py
+++ web_server/run.py
@@ -2,13 +2,23 @@
 import asyncio
 from app import create_app
 from app.utils.cloud_interface import Cloud_interface
-
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
+#chatgpt
+from flask import Flask
 
 #cloud interface (merros & tuya)
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 cloud = Cloud_interface(loop)
+
+# create a global variable to store the cloud object so controllers can use it
+global cloud_obj
+cloud_obj = cloud
+
+#chatgpt
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
+
+
