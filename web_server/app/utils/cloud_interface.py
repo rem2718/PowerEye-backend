3875d16668ab0b33c9a89d3d214f1013433c0c82
MayaKhalid <219410523@psu.edu.sa>
2023-10-21 14:15:00 +0300

    interface

--- web_server/app/utils/cloud_interface.py
+++ web_server/app/utils/cloud_interface.py
@@ -1,4 +1,3 @@
-# This file will be moved later on to its correct place
 from meross_iot.model.credentials import MerossCloudCreds
 from meross_iot.http_api import MerossHttpClient
 from meross_iot.manager import MerossManager
@@ -11,7 +10,6 @@ load_dotenv(dotenv_path)
 
 EMAIL = os.getenv("EMAIL")
 PASS = os.getenv("PASS")
-
 
 async def verify_credentials(email=None, password=None):
     client = await MerossHttpClient.async_from_user_password(email=email, password=password)
--- web_server/run.py
+++ web_server/run.py
@@ -1,10 +1,14 @@
 #run.py:
+import asyncio
 from app import create_app
-
+from cloud_interface import Cloud_interface
 
 
 if __name__ == '__main__':
     app = create_app()
     app.run()
 
-
+#cloud interface (merros & tuya)
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+cloud = Cloud_interface(loop)
