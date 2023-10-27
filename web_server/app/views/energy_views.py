42ee0b7fbcf9e02ff529f1f02049c9b9bc9a7dee
MayaKhalid <219410523@psu.edu.sa>
2023-10-27 22:45:44 +0300

    dummy

--- web_server/app/views/energy_views.py
+++ web_server/app/views/energy_views.py
@@ -1,15 +1,35 @@
 # app\views\energy_views.py
 # from app import app
-from flask import request
-from app.utils.dummy_controller import test_verify_credentials
+from flask import request, session, jsonify
+from app.utils.dummy_controller import test_verify_credentials_tuya, test_smartplugs_tuya
 # from app.controllers.energy_controller import get_appliance_daily_energy, get_appliance_weekly_energy, get_appliance_monthly_energy,get_appliance_yearly_energy, get_room_daily_energy,get_room_weekly_energy,get_room_monthly_energy,get_room_yearly_energy, get_total_daily_energy, get_total_weekly_energy,get_total_monthly_energy, get_total_yearly_energy
 from flask import Blueprint
+from markupsafe import escape
 
 app = Blueprint("main", __name__)
 
+@app.route('/')
+def index():
+    if 'user' in session:
+        return 'Logged in as %s' % escape(session['user'])
+    return 'You are not logged in'
+
 @app.route('/verify')
 def verify():
-    return test_verify_credentials()
+    return test_verify_credentials_tuya()
+
+@app.route('/smartplugs')
+def smartplugs():
+    return test_smartplugs_tuya()
+
+# @app.route('/get_smartplugs')
+# def getPlugs():
+#     return test_get_smartplugs()
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('user', None)  # Remove the 'user' key from the session
+#     return jsonify({'message': 'Logged out successfully'})
 
 # @app.route('/energy/appliance/<string:timeframe>/<int:timeSinceCurrent>', methods=['GET']) 
 # def appliance_energy_route(time_frame, time_since_current):
--- web_server/run.py
+++ web_server/run.py
@@ -1,14 +1,15 @@
 #run.py:
 from app import create_app
 from app.views.energy_views import app as dummy
-import asyncio
-from app.utils.cloud_interface import cloud_interface
 
+from flask import Flask
+from dotenv import load_dotenv
+import os
 
-#cloud interface (merros & tuya)
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-cloud = cloud_interface(loop)
+# Load environment variables from .env file
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 
 if __name__ == '__main__':
     app = create_app()
